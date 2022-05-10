c9f0c188fa8b27634a1b7b34c9bea7e0dcfbfca5
LyChristopherC <LyChristopherC@gmail.com>
2022-05-10 15:40:42 -0400

    Added Profile Page

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,37 @@
-import { supabase } from '../client';
+import { supabase } from '../client'
 
 export default function Profile({ session }) {
+  async function getProfile() {
+    try {
+      const user = supabase.auth.user()
+      let { data, error, status } = await supabase
+        .from('profiles')
+        .select(`username`)
+        .eq('id', user.id)
+        .single()
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setUsername(data.username)
+      }
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
-    <div className='container mx-auto grid place-content-center min-h-screen'>
+    <div className="container mx-auto grid min-h-screen place-content-center">
       <p>Oh hi there {session.user.email}</p>
+      {console.log(session, 'this is session')}
       <button
-        className='mt-4 p-2 pl-5 pr-5 bg-blue-500 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300'
+        className="mt-4 rounded-lg border-blue-300 bg-blue-500 p-2 pl-5 pr-5 text-lg text-gray-100 focus:border-4"
         onClick={() => supabase.auth.signOut()}
       >
         Logout
       </button>
     </div>
-  );
+  )
 }
