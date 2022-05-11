6ad6585dff3e3f1eccf730d2c1c7bbd7d84b0940
LyChristopherC <LyChristopherC@gmail.com>
2022-05-11 12:48:38 -0400

    Seperated admin chores and non admin chores

--- pages/chores.js
+++ pages/chores.js
@@ -1,51 +1,145 @@
-import { supabase } from '../client'
 import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { supabase } from '../client'
+import {
+  fetchSingleUserChores,
+  addChore,
+  deleteChore,
+} from '../store/features/chores'
 
-export default function Chores() {
-  const [chores, setChores] = useState([])
-  const [user, setUser] = useState(null)
+export default function AllDBChores() {
+  // **** Need to add due date to database ****
 
-  async function displayChores() {
-    const { data: chore } = await supabase.from('household_table').select(`
-      authUsers_id,
-      chores (
-        name
-      )
-      `)
+  //local state for controlled chore input form
+  const [chore, setChore] = useState({
+    name: '',
+    notes: '',
+    // xp: '',
+    // isComplete: '',
+  })
+  const { name, notes } = chore
 
-    const user = supabase.auth.user()
-    console.log(user)
+  //gets the list of chores and loading state from the redux store
+  let { singleChore } = useSelector((store) => store)
+  let [chores, loading] = [singleChore.entities, singleChore.loading]
+  console.log(chores, 'this is chores from store')
+  const dispatch = useDispatch()
+  //fetchAllChores gets chores in the database
+  useEffect(() => {
+    dispatch(fetchSingleUserChores())
+  }, [])
 
-    setChores(chore)
-    setUser(user)
+  // Dispatches new chores to the store
+  function dispatchChore() {
+    dispatch(addChore(chore))
+    // Reset the chore details & clears the form data
+    setChore({
+      name: '',
+      notes: '',
+      // xp: '',
+      // isComplete: '',
+    })
   }
 
-  useEffect(() => {
-    displayChores()
-  }, [])
+  // Display the spinner if loading
+  if (loading)
+    return (
+      <div className="flex items-center justify-center">
+        <div
+          className="
+    mt-36
+    h-32
+    w-32
+    animate-spin
+    rounded-full border-t-2 border-b-2 border-blue-500
+  "
+        ></div>
+      </div>
+    )
 
   return (
     <>
-      <h1>Chores</h1>
-      <ul>
-        {chores?.map((chore) => (
-          <>
-            {chore.authUsers_id === user?.id && (
-              <li key={chore.id}>
-                {chore.chores.name}
-                {/* <button
-                  onClick={() => {
-                    supabase.from('chores').delete().eq('id', chore.id)
-                    displayChores()
-                  }}
+      <div className="columns-1 flex-col">
+        <div
+          tabIndex="0"
+          className="collapse-arrow collapse rounded-box border border-base-300 bg-base-100"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Focus me to see content
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex="0" attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+
+        <form>
+          <div>
+            <label
+              className="mb-2 block text-sm font-bold text-gray-700"
+              htmlFor="choreName"
+            >
+              Chore Name
+            </label>
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              id="choreName"
+              type="text"
+              value={name.toString()}
+              onChange={(e) => setChore({ ...chore, name: e.target.value })}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="mb-2 block text-sm font-bold text-gray-700"
+              htmlFor="choreNotes"
+            >
+              Chore Notes
+            </label>
+
+            <textarea
+              className="form-textarea focus:shadow-outline mt-1 block w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+              rows="3"
+              placeholder="Chore Notes"
+              value={notes.toString()}
+              onChange={(e) => setChore({ ...chore, notes: e.target.value })}
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
+              type="button"
+              onClick={dispatchChore} // Call the addChore Function
+            >
+              Add Chore
+            </button>
+          </div>
+        </form>
+      </div>
+      {
+        //if there are chores, map through and display them
+        chores &&
+          chores.map((chore, index) => (
+            <tr key={chore.id}>
+              {/* {console.log(chores)} */}
+              <td className="border px-4 py-4">{index + 1}</td>
+              <td className="border px-4 py-4">{chore.chores.name}</td>
+              <td className="border px-8 py-4">{chore.chores.notes}</td>
+              <td className="border px-8 py-4">{chore.chores.dueDate}</td>
+              <td className="border px-8 py-4">{chore.chores.xp}</td>
+              <td className="border px-8 py-4">
+                {' '}
+                <button
+                  className="focus:shadow-outline rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700 focus:outline-none"
+                  type="button"
+                  // Deletes the chore
+                  onClick={() => dispatch(deleteChore(chore.id))}
                 >
                   Delete
-                </button> */}
-                </li>
-              )}
-            </>
-          ))}
-      </ul>
+                </button>
+              </td>
+            </tr>
+          ))
+      }
     </>
   )
 }
---
+++ store/features/chores.js
@@ -0,0 +1,98 @@
+import { createAction, createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { supabase } from '../../client'
+
+const initialState = {
+  entities: [],
+  loading: false,
+}
+
+// *** THUNKS *** //
+
+//Fetches all chores associated to that user only
+export const fetchSingleUserChores = createAsyncThunk(
+  //action type string
+  'chores/fetchAllChores',
+  //callback function
+  async (thunkAPI) => {
+    const user = supabase.auth.user()
+    try {
+      let { data: userID } = await supabase
+        .from('profiles')
+        .select(`id`)
+        .eq('id', user.id)
+        .single()
+
+      let { data: chores } = await supabase
+        .from('responsibility')
+        .select(
+          `chore_id, chores (
+          *
+        ) `
+        )
+        .eq('profile_id', userID.id)
+      console.log(userID.id, 'this is user')
+
+      return chores
+    } catch (error) {
+      console.log(error)
+      return error
+    }
+  }
+)
+
+// export const addChore = createAsyncThunk(
+//   'chores/addChore',
+//   async (chore, thunkAPI) => {
+//     try {
+//       let { name, notes } = chore
+//       //add the chore to the database
+//       await supabase
+//         .from('chores') // Select the Table
+//         .insert([
+//           {
+//             name,
+//             notes,
+//             // xp,
+//             // isComplete,
+//           },
+//         ])
+//       //dispatch fetchALlChores to update the state from db
+//       thunkAPI.dispatch(fetchAllChores())
+//     } catch (error) {
+//       console.log(error)
+//       return error
+//     }
+//   }
+// )
+
+// export const deleteChore = createAsyncThunk(
+//   'chores/deleteChore',
+//   async (choreId, thunkAPI) => {
+//     try {
+//       //delete the chore
+//       await supabase.from('chores').delete().eq('id', choreId)
+//       //dispatch fetchALlChores to update the state from db
+//       thunkAPI.dispatch(fetchAllChores())
+//     } catch (error) {
+//       console.log(error)
+//       return error
+//     }
+//   }
+// )
+
+// ***Slice Creator*** //
+const singleUserChoresSlice = createSlice({
+  name: 'singleUserChores',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSingleUserChores.fulfilled, (state, action) => {
+        state.loading = false
+        state.entities = action.payload
+      })
+      .addDefaultCase((state, action) => {})
+  },
+})
+
+export default singleUserChoresSlice.reducer
