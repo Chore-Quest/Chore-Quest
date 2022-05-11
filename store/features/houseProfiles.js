caf06d3c66c1b0c6d12dcf49f4c28615b70d13ca
demetrius robinson <demetrius.robinson@icloud.com>
2022-05-11 15:51:02 -0400

    added household thunk to fetch all applicable profiles

---
+++ store/features/houseProfiles.js
@@ -0,0 +1,51 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { supabase } from '../../client'
+
+const initialState = {
+  entities: [],
+  loading: false,
+}
+
+// *** THUNKS *** //
+// fetch all profiles from a household
+export const fetchAllProfiles = createAsyncThunk(
+  'profiles/fetchAllProfiles',
+  async (thunkAPI) => {
+    const user = supabase.auth.user()
+    try {
+      let { data: houseHoldId } = await supabase
+        .from('profiles')
+        .select('household_id')
+        .eq('id', user.id)
+        .single()
+
+      let { data: profiles } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('household_id', houseHoldId.household_id)
+      console.log('*******************')
+      console.log(profiles, 'from fetch all profiles thunk')
+      console.log('*******************')
+      return profiles
+    } catch (error) {
+      console.log(error)
+      return error
+    }
+  }
+)
+
+// *** SLICES *** //
+const profilesSlice = createSlice({
+  name: 'profiles',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProfiles.fulfilled, (state, action) => {
+        state.entities = action.payload
+        state.loading = false
+      })
+      .addDefaultCase((state, action) => {})
+  },
+})
+export default profilesSlice.reducer
--- store/store.js
+++ store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import choresReducer from './features/adminChores'
 import singleChoresReducer from './features/chores'
 import singleProfileReducer from './features/singleProfile'
+import singleHouseholdReducer from './features/singleHousehold'
 
 const store = configureStore({
   reducer: {
     allChores: choresReducer,
     singleChore: singleChoresReducer,
     singleProfile: singleProfileReducer,
+    singleHouseholdProfiles: singleHouseholdReducer,
   },
 })
 
