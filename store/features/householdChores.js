dea46f3a024714ccde636c647d54ff5b38825a7a
pmentropy <justin.duplain@gmail.com>
2022-05-11 15:45:54 -0400

    updates createChore thunk

--- store/features/householdChores.js
+++ store/features/householdChores.js
@@ -8,12 +8,12 @@ const initialState = {
 
 // *** THUNKS *** //
 
-//ADMIN FETCH ALL CHORES FOR HOUSEHOLD
+//FETCH ALL CHORES FOR HOUSEHOLD
 export const fetchAllChores = createAsyncThunk(
   //action type string
   'chores/fetchAllChores',
   //callback function
-  async (thunkAPI) => {
+  async () => {
     const user = supabase.auth.user()
     try {
       let { data: houseHoldId } = await supabase
@@ -30,7 +30,6 @@ export const fetchAllChores = createAsyncThunk(
         ) `
         )
         .eq('household_id', houseHoldId.household_id)
-
       return chores
     } catch (error) {
       console.log(error)
@@ -39,22 +38,31 @@ export const fetchAllChores = createAsyncThunk(
   }
 )
 
-export const addChore = createAsyncThunk(
-  'chores/addChore',
+//ADDS A CHORE FOR THE USER'S HOSUEHOLD
+export const createChore = createAsyncThunk(
+  'chores/createChore',
   async (chore, thunkAPI) => {
+    const user = supabase.auth.user()
     try {
+      let { data } = await supabase
+        .from('profiles')
+        .select(`household_id`)
+        .eq('id', user.id)
+      const household_id = data.household_id
       let { name, notes } = chore
       //add the chore to the database
-      await supabase
-        .from('chores') // Select the Table
-        .insert([
-          {
-            name,
-            notes,
-            // xp,
-            // isComplete,
-          },
-        ])
+      const res = await supabase.from('chores').insert([
+        {
+          name,
+          notes,
+          household_id,
+        },
+      ])
+      console.log('***************')
+      console.log('Server Response from createChore Thunk:')
+      console.log(res)
+      console.log('createChore Thunk Says: "Fetching All Household Chores"')
+      console.log('***************')
       //dispatch fetchALlChores to update the state from db
       thunkAPI.dispatch(fetchAllChores())
     } catch (error) {
--- store/store.js
+++ store/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-import choresReducer from './features/adminChores'
-import singleChoresReducer from './features/chores'
+import choresReducer from './features/householdChores'
+import singleChoreReducer from './features/singleChore'
 
 const store = configureStore({
   reducer: {
     allChores: choresReducer,
-    singleChore: singleChoresReducer,
+    singleChore: singleChoreReducer,
   },
 })
 
