a003d804d7afb36adebbebb1383fbbce335143ee
demetrius robinson <demetrius.robinson@icloud.com>
2022-05-11 17:47:04 -0400

    added some styles to profile view

--- pages/profile.js
+++ pages/profile.js
@@ -18,23 +18,26 @@ export default function Profile({ session }) {
 
   return (
     <div className="container mx-auto grid min-h-screen place-content-center">
-      <p>Oh hi there {profile.username}</p>
-      {console.log(profile, 'this is profile')}
-      <div className="justifyCenter mask mask-hexagon box-content h-64 w-64">
-        <img src="https://api.lorem.space/image/face?hash=33791" />
+      <div className="card w-96 bg-base-100 p-10 shadow-xl">
+        <p className="mx-auto">Oh hi there {profile.username}</p>
+        {console.log(profile, 'this is profile')}
+        <div className="justifyCenter mask mask-hexagon mx-auto box-content h-64 w-64">
+          <img src={profile.avatar_url} />
+        </div>
+        <h1 className="mx-auto">XP:{profile.personalXP}</h1>
+        <button
+          className="mt-4 rounded-lg border-blue-300 bg-blue-500 p-2 pl-5 pr-5 text-lg text-gray-100 focus:border-4"
+          onClick={() => router.push('/editprofile')}
+        >
+          Edit Profile
+        </button>
+        <button
+          className="mt-4 rounded-lg border-blue-300 bg-blue-500 p-2 pl-5 pr-5 text-lg text-gray-100 focus:border-4"
+          onClick={() => supabase.auth.signOut()}
+        >
+          Logout
+        </button>
       </div>
-      <button
-        className="mt-4 rounded-lg border-blue-300 bg-blue-500 p-2 pl-5 pr-5 text-lg text-gray-100 focus:border-4"
-        onClick={() => router.push('/editprofile')}
-      >
-        Edit Profile
-      </button>
-      <button
-        className="mt-4 rounded-lg border-blue-300 bg-blue-500 p-2 pl-5 pr-5 text-lg text-gray-100 focus:border-4"
-        onClick={() => supabase.auth.signOut()}
-      >
-        Logout
-      </button>
     </div>
   )
 }
