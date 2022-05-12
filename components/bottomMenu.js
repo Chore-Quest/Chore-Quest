7dd7d463436ddc619317f286a3d8a1f7539f0b03
demetrius robinson <demetrius.robinson@icloud.com>
2022-05-12 12:44:12 -0400

    thers a wizard

--- components/bottomMenu.js
+++ components/bottomMenu.js
@@ -4,24 +4,26 @@ import Link from 'next/link'
 export default function BottomMenu() {
   return (
     <ul className="menu menu-horizontal fixed bottom-0 left-0 right-0 w-full justify-between rounded-t-lg bg-base-300 px-6">
-      <li>
-        <a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-            />
-          </svg>
-        </a>
-      </li>
+      <Link href="/">
+        <li>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+          </a>
+        </li>
+      </Link>
       <li>
         <a>
           <svg
