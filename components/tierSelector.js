b9de3230d51092de9108209e54e49b88b8fa4dbe
LyChristopherC <LyChristopherC@gmail.com>
2022-05-13 00:20:37 -0400

    Tiers are added and sorted

---
+++ components/tierSelector.js
@@ -0,0 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchAllItems } from '../store/features/itemTiers'
+
+export default function TierSelector() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchAllItems())
+  }, [])
+
+  const { allItems } = useSelector((store) => store)
+  const { entities: items } = allItems
+
+  // const newArr = items.sort((a, b) => a.xp - b.xp)
+  // const sortItems = (array) => {
+
+  //   return newArr
+  // }
+
+  // console.log(items.sort((a, b) => a.xp - b.xp))
+
+  console.log(items, 'items')
+  return (
+    <select className="select w-full max-w-xs">
+      <option disabled selected>
+        Select Tier
+      </option>
+      {items.map((item) => (
+        <option key={item.id}>{`Tier: ${item.tier} ${item.name}`}</option>
+      ))}
+    </select>
+  )
+}
