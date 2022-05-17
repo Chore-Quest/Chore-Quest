f6e11b37940c8c619b0aa84be436f873ea4da59d
demetrius robinson <demetrius.robinson@icloud.com>
2022-05-17 10:55:19 -0400

    getting ready to merge after starting single profile view

--- pages/addchore.js
+++ pages/addchore.js
@@ -82,7 +82,7 @@ export default function AddChore() {
         </label>
 
         <textarea
-          className="form-textarea frosted focus:shadow-outline mt-1 block w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
+          className="form-textarea frosted focus:shadow-outline mt-1 block w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-400 shadow focus:outline-none"
           rows="3"
           placeholder="Chore Notes"
           value={notes.toString()}
