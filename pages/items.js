7cb4721b62bf794082d0e1bd4ec2f6618a4b5672
Spencer Hamilton <speham@gmail.com>
2022-05-19 11:19:34 -0500

    created items component. User items now displaying properly

--- pages/items.js
+++ pages/items.js
@@ -1,28 +1,12 @@
-import { supabase } from '../client'
-import { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useRouter } from 'next/router'
+import Items from '../components/items'
 
-export default function items() {
-  const getItems = async () => {
-    // const user = supabase.auth.user()
-    let { data } = await supabase.from('items').select(`
-  name,
-  user_items (
-    item_id,
-    quantity
-  )
-`)
-
-    const hasQuantity = (item) => item.quantity
-    let items = data.filter((item) => item.user_items.some(hasQuantity))
-    console.log(items)
-  }
-
-  getItems()
+export default function UserItems() {
+  const router = useRouter()
 
   return (
     <>
-      <div>{items && items.map((item) => <div>{item.name}</div>)}</div>
+      <Items />
     </>
   )
 }
