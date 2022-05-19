dd2b07faa2f8d00054ad34dbb4daafd113e913ff
LyChristopherC <LyChristopherC@gmail.com>
2022-05-19 16:49:57 -0400

    Adding item and xp update features work

--- store/features/itemInventory.js
+++ store/features/itemInventory.js
@@ -46,6 +46,44 @@ export const fetchUserItems = createAsyncThunk(
     }
   }
 )
+
+export const assignUsersItem = createAsyncThunk(
+  //action type string
+  'item/assignUsersItem',
+  //callback function
+  async ({ itemID, assignedProfiles }) => {
+    // const user = supabase.auth.user()
+    try {
+      //get the householdID
+      let { data: item } = await supabase
+        .from('items')
+        .select('*')
+        .eq('id', itemID)
+        .single()
+      console.log(assignedProfiles, 'this is assigned profiles')
+      console.log(item, 'this is item from thunk')
+      // assignedProfiles.map(async (profile) => {
+      // })
+      assignedProfiles.map(async (profile) => {
+        console.log(profile.profiles.personalXP, item.xp, 'adding xp here')
+        const { data: something } = await supabase
+          .from('user_items')
+          .insert([
+            { item_id: item.id, profile_id: profile.profile_id, xp: item.xp },
+          ])
+        const { data } = await supabase
+          .from('profiles')
+          .update({ personalXP: profile.profiles.personalXP + item.xp })
+          .eq('id', profile.profile_id)
+      })
+      // return item
+    } catch (error) {
+      console.log(error)
+      return error
+    }
+  }
+)
+
 // export const fetchUserItems = createAsyncThunk(
 //   'item/fetchUserItems',
 //   async (thunkAPI) => {
