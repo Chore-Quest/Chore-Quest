9438964cdf8901a059c66ebdf64c560d4164452d
Illmaticno1 <Demetrius.robinson@icloud.com>
2022-05-20 12:32:01 -0400

    added improvements to mobile view

--- components/items.js
+++ components/items.js
@@ -18,30 +18,23 @@ export default function Items() {
       <h1 className="mb-3 flex items-center justify-around text-center text-4xl font-extrabold uppercase mix-blend-lighten">
         inventory
       </h1>
-      <div className="frosted flex flex-wrap rounded-2xl border-slate-900 md:p-5">
+      <div className="frosted flex shrink flex-wrap rounded-2xl border-slate-900 md:p-5">
         {items.entities.map((item) => (
           <>
-            {/* <div
-              key={item.id}
-              className="relative flex cursor-grab flex-col items-center justify-center rounded-xl border border-white p-5 text-white"
-            >
-              <h2>{item.items.name}</h2>
-              <img src={item.items.imageURL} className=" object-scale-down" />
-            </div> */}
-            <div className="mx-1 my-1 flex shrink cursor-grab flex-col rounded-xl border-inherit bg-black drop-shadow-2xl md:mx-auto md:my-auto md:mt-5 md:h-96 md:w-72">
+            <div className="mx-1 my-1 flex h-24 w-20 cursor-grab flex-col rounded-xl border-inherit bg-black p-2 drop-shadow-2xl md:mx-auto md:my-auto md:mt-5 md:h-96 md:w-72">
               <div className="relative w-full flex-1 flex-col items-center justify-end p-4">
                 <div className="absolute top-0 left-0 min-h-full min-w-full overflow-hidden rounded-tr-xl">
-                  <div className="absolute -right-28 -top-10 z-10 rounded-full bg-gradient-to-tr from-stone-900 to-red-900 md:h-96 md:w-96"></div>
+                  <div className="absolute z-10 rounded-full bg-gradient-to-tr from-stone-900 to-red-900 md:-right-28 md:-top-10 md:h-96 md:w-96"></div>
                   <img
                     src={item.items.imageURL}
-                    className="fixed z-40 rotate-12 object-scale-down"
+                    className="fixed z-40 object-scale-down md:rotate-12"
                   />
 
-                  <div className="absolute bottom-0 flex border-yellow-300 md:p-5">
-                    <h1 className="relative z-40 m-10 bg-gradient-to-r from-amber-200 to-amber-700  bg-clip-text text-4xl  font-extrabold uppercase text-transparent drop-shadow-2xl">
+                  <div className="absolute flex shrink items-center justify-center border-yellow-300 pt-3 md:bottom-0 md:p-5">
+                    <h1 className="relative z-40 shrink bg-gradient-to-r from-amber-200 to-amber-700 bg-clip-text text-sm font-extrabold uppercase leading-none text-transparent drop-shadow-2xl md:m-10 md:text-4xl">
                       {item.items.name}
                       <br />
-                      <span className="">+{item.items.xp} XP</span>
+                      <span className="shrink">+{item.items.xp} XP</span>
                     </h1>
                   </div>
                 </div>
