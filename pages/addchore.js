c574570bd97f59163d5524d22bfd648e5db01f78
pmentropy <justin.duplain@gmail.com>
2022-05-21 13:11:35 -0400

    Add a Chore title

--- pages/addchore.js
+++ pages/addchore.js
@@ -50,75 +50,78 @@ export default function AddChore() {
     })
   }
   return (
-    <form className="h-screen">
-      <div>
-        <label
-          className="mb-2 block text-sm font-bold text-gray-700"
-          htmlFor="choreName"
+    <>
+      <h1 className="pb-8 text-center text-4xl">Add a Chore</h1>
+      <form className="h-screen">
+        <div>
+          <label
+            className="mb-2 block text-sm font-bold text-gray-400"
+            htmlFor="choreName"
+          >
+            Chore Name
+          </label>
+
+          <input
+            className="frosted focus:shadow-outline  mb-6 w-full appearance-none rounded border py-2 px-3 leading-tight  text-gray-400 shadow focus:outline-none"
+            id="choreName"
+            type="text"
+            value={name.toString()}
+            onChange={(e) => setChore({ ...chore, name: e.target.value })}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="mb-2 block text-sm font-bold text-gray-400"
+            htmlFor="choreNotes"
+          >
+            Chore Notes
+          </label>
+
+          <textarea
+            className="form-textarea frosted focus:shadow-outline mt-1 mb-6 block w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-400 shadow focus:outline-none"
+            rows="3"
+            value={notes.toString()}
+            onChange={(e) => setChore({ ...chore, notes: e.target.value })}
+          ></textarea>
+        </div>
+        <select
+          className="select mb-4 mr-3 w-full max-w-xs"
+          onChange={(e) => setChore({ ...chore, item: e.target.value })}
         >
-          Chore Name
-        </label>
+          <option defaultValue>Select Tier</option>
+          {items.map((item) => (
+            <option
+              key={item.id}
+              value={item.id}
+            >{`Tier: ${item.tier} ${item.name} ${item.xp} XP`}</option>
+          ))}
+        </select>
 
-        <input
-          className="frosted focus:shadow-outline  mb-6 w-full appearance-none rounded border py-2 px-3 leading-tight  text-gray-400 shadow focus:outline-none"
-          id="choreName"
-          type="text"
-          value={name.toString()}
-          onChange={(e) => setChore({ ...chore, name: e.target.value })}
-        />
-      </div>
-      <div className="mb-4">
-        <label
-          className="mb-2 block text-sm font-bold text-gray-400"
-          htmlFor="choreNotes"
+        <select
+          className="select mb-2 w-full max-w-xs"
+          onChange={(e) => setChore({ ...chore, profile_id: e.target.value })}
         >
-          Chore Notes
-        </label>
+          <option defaultValue value="">
+            Assign To
+          </option>
+          {profiles.entities?.map((profile) => (
+            <option
+              key={profile.id}
+              value={profile.id}
+            >{`${profile.username}`}</option>
+          ))}
+        </select>
 
-        <textarea
-          className="form-textarea frosted focus:shadow-outline mt-1 mb-6 block w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-400 shadow focus:outline-none"
-          rows="3"
-          value={notes.toString()}
-          onChange={(e) => setChore({ ...chore, notes: e.target.value })}
-        ></textarea>
-      </div>
-      <select
-        className="select mb-4 mr-3 w-full max-w-xs"
-        onChange={(e) => setChore({ ...chore, item: e.target.value })}
-      >
-        <option defaultValue>Select Tier</option>
-        {items.map((item) => (
-          <option
-            key={item.id}
-            value={item.id}
-          >{`Tier: ${item.tier} ${item.name} ${item.xp} XP`}</option>
-        ))}
-      </select>
-
-      <select
-        className="select mb-2 w-full max-w-xs"
-        onChange={(e) => setChore({ ...chore, profile_id: e.target.value })}
-      >
-        <option defaultValue value="">
-          Assign To
-        </option>
-        {profiles.entities?.map((profile) => (
-          <option
-            key={profile.id}
-            value={profile.id}
-          >{`${profile.username}`}</option>
-        ))}
-      </select>
-
-      <div className="flex items-center justify-between">
-        <button
-          className="focus:shadow-outline rounded bg-gray-500 py-2 px-4 font-bold text-white hover:bg-gray-800 focus:outline-none"
-          type="button"
-          onClick={handleAddChore} // Call the addChore Function
-        >
-          Add Chore
-        </button>
-      </div>
-    </form>
+        <div className="flex items-center justify-between">
+          <button
+            className="focus:shadow-outline rounded bg-gray-500 py-2 px-4 font-bold text-white hover:bg-gray-800 focus:outline-none"
+            type="button"
+            onClick={handleAddChore} // Call the addChore Function
+          >
+            Add Chore
+          </button>
+        </div>
+      </form>
+    </>
   )
 }
